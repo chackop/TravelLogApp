e44786524ebebb1ec45d954f543569e6c250e698
Chacko <chackop@gmail.com>
2019-01-11 00:25:56 +0000

    added RCN navigation

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 /** @format */
 
 import React from 'react';
-import { AppRegistry } from 'react-native';
+// import { AppRegistry } from 'react-native';
+import { Navigation } from "react-native-navigation";
+
 import { Provider } from 'react-redux';
 import App from './App';
 import configureStore from './src/store/configureStore';
-import {name as appName} from './app.json';
+// import {name as appName} from './app.json';
+
 
 const store = configureStore();
 
@@ -15,5 +18,16 @@ const RNRedux = () => (
     </Provider>
 );
 
-AppRegistry.registerComponent(appName, () => RNRedux);
+// AppRegistry.registerComponent(appName, () => RNRedux);
+Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => RNRedux);
+
+Navigation.events().registerAppLaunchedListener(() => {
+      Navigation.setRoot({
+        root: {
+          component: {
+            name: "navigation.playground.WelcomeScreen"
+          }
+        }
+      });
+    });
 
