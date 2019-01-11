8d21fff8b8bacad0730ecd3316c743d73a94a608
Chacko <chackop@gmail.com>
2019-01-11 00:54:11 +0000

    auth screen start

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 /** @format */
 
-import React from 'react';
+// import React from 'react';
 // import { AppRegistry } from 'react-native';
-import { Navigation } from "react-native-navigation";
+// import { Navigation } from "react-native-navigation";
 
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import App from './App';
-import configureStore from './src/store/configureStore';
+// import configureStore from './src/store/configureStore';
 // import {name as appName} from './app.json';
 
 
-const store = configureStore();
+// const store = configureStore();
 
-const RNRedux = () => (
-    <Provider store={store}>
-        <App />
-    </Provider>
-);
+// const RNRedux = () => (
+//     <Provider store={store}>
+//         <App />
+//     </Provider>
+// );
 
-// AppRegistry.registerComponent(appName, () => RNRedux);
-Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => RNRedux);
+// AppRegistry.registerComponent(appName, () => App);
+// Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => RNRedux);
 
-Navigation.events().registerAppLaunchedListener(() => {
-      Navigation.setRoot({
-        root: {
-          component: {
-            name: "navigation.playground.WelcomeScreen"
-          }
-        }
-      });
-    });
+// Navigation.events().registerAppLaunchedListener(() => {
+//       Navigation.setRoot({
+//         root: {
+//           component: {
+//             name: "navigation.playground.WelcomeScreen"
+//           }
+//         }
+//       });
+//     });
 
---
+++ src/screens/Auth/Auth.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class AuthScreen extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//     };
+//   }
+
+  render() {
+    return (
+      <View>
+        <Text> In Auth Screen </Text>
+      </View>
+    );
+  }
+}
+
+export default AuthScreen;
