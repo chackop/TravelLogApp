2b2d3d0e574e6d5b8002682a6ede2166cb236ead
Chacko <chackop@gmail.com>
2019-01-13 00:06:53 +0000

    add icon

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -4,12 +4,7 @@ import { View, Text, Button } from 'react-native';
 import { startTabs, goHome } from "../MainTabs/startMainTabs";
 
 class AuthScreen extends Component {
-    //   constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //     };
-    //   }
-
+    
     loginHandler = () => {
         // goHome();
         startTabs();
--- src/screens/MainTabs/startMainTabs.js
+++ src/screens/MainTabs/startMainTabs.js
@@ -1,56 +1,69 @@
 import { Navigation } from 'react-native-navigation';
+import { Icon } from 'react-native-vector-icons/Ionicons';
 
-const bottomTabs = {
-    children: [
-        {
-            component: {
-                name: 'travellogapp.SharedPlaceScreen',
-                options: {
-                    bottomTab: {
-                        text: 'Shared Place'
+
+
+export const startTabs = () => {
+    Promise.all([
+        Icon.getImageSource("map", 30),
+        Icon.getImageSource("share", 30)
+    ])
+        .then(
+            sources => {
+                Navigation.setRoot({
+                    root: {
+                        bottomTabs: {
+                            children: [
+                                {
+                                    component: {
+                                        name: 'travellogapp.SharedPlaceScreen',
+                                        options: {
+                                            bottomTab: {
+                                                text: 'Shared Place',
+                                                icon: sources[0]
+                                            }
+                                        }
+                                    }
+                                },
+                                {
+                                    component: {
+                                        name: 'travellogapp.FindPlaceScreen',
+                                        options: {
+                                            bottomTab: {
+                                                text: 'Find Place',
+                                                icon: sources[1]
+                                            }
+                                        }
+                                    }
+                                }
+                            ],
+                            options: {}
+                        }
                     }
-                }
+                });
             }
-        },
-        {
-            component: {
-                name: 'travellogapp.FindPlaceScreen',
-                options: {
-                    bottomTab: {
-                        text: 'Find Place'
-                    }
-                }
-            }
-        }
-    ],
-    options: {}
+        );
 }
 
-export const startTabs = () => Navigation.setRoot({
-    root: {
-        bottomTabs
-    }
-});
-
-export const goHome = () => Navigation.setRoot({
-    root: {
-        stack: {
-            id: 'App',
-            children: [
-                {
-                    component: {
-                        name: 'travellogapp.SharedPlaceScreen',
-                        options: {
-                            bottomTab: {
-                                text: 'Shared Place',
-                            }
-                        }
-                    },
-                }
-            ],
-        }
-    }
-});
+// export const goHome = () => Navigation.setRoot({
+//     root: {
+//         stack: {
+//             id: 'App',
+//             children: [
+//                 {
+//                     component: {
+//                         name: 'travellogapp.SharedPlaceScreen',
+//                         options: {
+//                             bottomTab: {
+//                                 text: 'Shared Place',
+//                             }
+//                         }
+//                     },
+//                 }
+//             ],
+//         }
+//     }
+// });
 
 
 // export default startTabs;
